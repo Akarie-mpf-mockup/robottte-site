246acba87deb84a19bad56d74812ad511c5f8f11
kenichi.takahashi.kojin@gmail.com <kenichi.takahashi.kojin@gmail.com>
2026-03-19 16:34:45 +0900

    登録

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,9 @@ export default function App() {
       <Hero />
       <Why />
       <Products />
-      <News />
       <Team />
       <About />
+      <News />
       <Careers />
       <Contact />
       <Footer />
